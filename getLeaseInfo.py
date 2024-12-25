21e5da8adcc3a96354a85166d53c4c362d19d495
Amitkumaryadav82 <49905324+Amitkumaryadav82@users.noreply.github.com>
2024-12-25 13:49:18 +0000

    Updated code to provide proper answer as output

--- getLeaseInfo.py
+++ getLeaseInfo.py
@@ -205,12 +205,13 @@ def invoke_chain(request,clf_label,clf_chain,sql_code_chain,rag_chain):
     elif "non sql" in clf_label.lower():
         print(f" inside non sql...Called rag_chain")
         raw_output=rag_chain.invoke({"input": request})
-        print("*****raw output ", raw_output)
+        print("*****raw output ", raw_output["answer"])
         # Accessing the page_content attribute of the Document object
+        answer=raw_output["answer"]
         page_content = raw_output["context"][0].page_content
         # Creating a dictionary with the page_content
         page_content_json = {"page_content": page_content }
-        output = json.dumps(page_content_json, indent=4)
+        output = json.dumps(answer, indent=4)
         print(output)
         
     else:
