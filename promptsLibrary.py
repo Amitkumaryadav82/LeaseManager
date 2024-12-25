ee8d155444a4be1a3960088cc502cc26e61e1847
Amitkumaryadav82 <49905324+Amitkumaryadav82@users.noreply.github.com>
2024-12-25 12:23:46 +0000

    further updated prompts

--- promptsLibrary.py
+++ promptsLibrary.py
@@ -57,7 +57,7 @@ You are a MySQL expert. Given an input request, return a syntactically correct M
 - No New Columns or Aggregations: Do not return any new columns nor perform aggregation on columns. Return only the columns present in tables; further aggregations will be done by Python code in later steps.
 - Python Code: Python code should be well formatted and must not have any indentation or syntactical errors.
 - Database Connection Info: Information required to connect to the database like host, user, password, and database name are mentioned in settings.txt file.
-- Query Format: The query should be generated in a single line.
+- Query Format: The query should be generated in a single line and must be writted so that the qury can be executed on MYSQL database without any modifications.
 
 Answer in the following format only:
 SQLQuery: Generated SQL Query here
