23de1b6a57dc71e99157eab908abc59f9bffadb7
Amitkumaryadav82 <49905324+Amitkumaryadav82@users.noreply.github.com>
2024-12-30 05:45:20 +0000

    Added code for evaluation. Still not working

--- evaluation.py
+++ evaluation.py
@@ -5,7 +5,7 @@ from PIL import Image
 from deepeval import evaluate
 from deepeval.dataset import EvaluationDataset
 from deepeval.test_case import LLMTestCase
-from deepeval.metrics import ContextualPrecisionMetric, ContextualRecallMetric, ContextualRelevancyMetric
+from deepeval.metrics import  AnswerRelevancyMetric
 from langchain_anthropic import ChatAnthropic
 from langchain_aws import BedrockEmbeddings
 import boto3
@@ -16,6 +16,7 @@ import dash_html_components as html
 import tempfile
 import logging
 from utils import get_anthropic_llm, getSettings
+from CustomLLMEval import getCustomLLM
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
@@ -36,7 +37,15 @@ def extract_text_from_image(image_path):
 
 # Function to prepare the dataset from images stored in S3 bucket
 def prepare_dataset_from_s3(bucket_name, file_keys):
-    data = {'query': [], 'response': []}
+    data = {'query': ["for the lease agreement with M.A.S. REAL ESTATE SERVICES LTD., what is the notice period for termination of lease",
+                    "What is the  approved Tenant Improvement Budget for the lease with High Range SV1 LLC for the property in Scottsdale, Arizona",
+                    "For Lease GS-09B-02614, where do I need to submit the invoices?"
+
+                        ],
+             'response': ["as per lease GS-09B-02625,lease can be terminated in partial or whole anytime after 10th year from the day of commencement by providing 90 days notices to Lessor",
+                        " As per lease GS-09B-02614, the Tentant improvement Budget is $1,311,799.00",
+                        "As per lease GS-09B-002614, invoices should be shall be submitted at: GSA  Office Of Finance, P.O. Box 17181, Fort Worth, TX-760102-0181"
+             ]}
     
     with tempfile.TemporaryDirectory() as temp_dir:
         for file_key in file_keys:
@@ -67,21 +76,32 @@ def create_test_cases(df):
             expected_output=row['response']  # Assuming the extracted text is the expected output
         )
         test_cases.append(test_case)
+    print("******** TEst cases created successfully")
     return test_cases
 
 # Function to run the evaluation
 def run_evaluation(test_cases):
-    eval_dataset = EvaluationDataset(test_cases=test_cases)
-    
-    metrics = [
-        ContextualPrecisionMetric(),
-        ContextualRecallMetric(),
-        ContextualRelevancyMetric()
-    ]
+    print("*** initializing Test cases")
+    print("Eval DAta set Initatized")
+    # metrics = [
+    #     ContextualPrecisionMetric(),
+    #     ContextualRecallMetric(),
+    #     ContextualRelevancyMetric()
+    # ]
     
     evaluator_llm = get_anthropic_llm()
+    # evaluator_llm=getCustomLLM()
     bedrock = boto3.client(service_name="bedrock-runtime")
-    evaluator_embeddings = BedrockEmbeddings(model_id=settings.get('embedding_model'), client=bedrock)
+    evaluator_embeddings = BedrockEmbeddings(model_id=settings.get('embedding_model'),
+     client=bedrock)
+
+    print("***Settings done")
+    eval_dataset = EvaluationDataset(test_cases=test_cases)
+    print("******dataset done")
+    metric = AnswerRelevancyMetric(model="claude-3-opus-20240229")
+    print("Metric set")
+    result1= metric.measure(...)
+    print("Result1: ", result1)
 
     results = evaluate(dataset=eval_dataset, metrics=metrics)
     
@@ -113,9 +133,8 @@ def create_dashboard(results_df):
 def getEvalMetrics():
     bucket_name = 'capleasemanager'
     file_keys = [
-        'lease/gsa_(R)LDC02066-Lease-SF2-01.png', 'lease/gsa_(R)LDC02066-Lease-SF2-02.png',
-        'lease/gsa_(R)LDC02066-Lease-SF2-03.png', 'lease/gsa_(R)LDC02066-Lease-SF2-04.png',
-        'lease/gsa_(R)LDC02066-Lease-SF2-05.png'
+        'lease/gsa_LAZ02625-Lease-1_Z-01.png', 'lease/gsa_LAZ02625-Lease-1_Z-02.png',
+        'gsa_LAZ02614-SLA-2-_Z-01.png','gsa_LAZ02614-SLA-2-_Z-02'
     ]
     
     df = prepare_dataset_from_s3(bucket_name, file_keys)
@@ -123,7 +142,7 @@ def getEvalMetrics():
     
     test_cases = create_test_cases(df)
     results_df = run_evaluation(test_cases)
-    print(results_df.head())
+    print("************* ",results_df.head())
     
     create_dashboard(results_df)
 
